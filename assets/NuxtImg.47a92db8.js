f00a1b673695d50a1b4ba26136ccaee8e125877f
justi <justine.pepin.1@umontreal.ca>
2023-09-09 22:48:06 -0700

    Updates

---
+++ assets/NuxtImg.47a92db8.js
@@ -0,0 +1,1 @@
+import{a1 as f}from"./entry.60b7c2d6.js";export{f as default};
---
+++ assets/SourceLink.bfd89a39.js
@@ -0,0 +1,1 @@
+import o from"./ProseP.2d7b9286.js";import{d as r,b as t,X as n}from"./entry.60b7c2d6.js";const m=r({__name:"SourceLink",props:{source:{type:String,required:!0}},setup(_){return(c,p)=>{const e=o;return t(),n(e)}}});export{m as default};
