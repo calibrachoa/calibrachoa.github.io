dadfed20c3b4bdbf1e416a872b3c234d505c14d5
justi <justine.pepin.1@umontreal.ca>
2023-09-10 22:13:31 -0700

    changed description of home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,11 +15,5 @@ export default defineNuxtConfig({
   css: [
     '@/node_modules/lite-youtube-embed/src/lite-yt-embed.css'
   ],
-  plugins: [ {src: '@/plugins/youtube.client.js', ssr: false} ],
-  runtimeConfig: {
-    public: {
-      FORMSPREE_URL: '1369225.jp@gmail.com'
-    }
-  }
-  
+  plugins: [ {src: '@/plugins/youtube.client.js', ssr: false} ]  
 })
