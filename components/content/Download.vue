e9bb3e254795e8c00bf6efa0b8455f5759c3788d
justi <justine.pepin.1@umontreal.ca>
2024-05-12 20:59:55 -0700

    adding resume link

---
+++ components/content/Download.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="download">
+    <a :href="filename">
+      {{ label }}
+    </a>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      filename: {
+        type: String,
+        required: true
+      },
+      label: {
+        type: String,
+        required: false,
+        default: "You can download my resume by clicking here!"
+      },
+    },
+    fetchOnServer: false,
+  }
+</script>
