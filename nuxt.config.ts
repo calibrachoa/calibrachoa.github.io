825295cc0a1bd6ea59458a6308aaa56f44ed7031
justi <justine.pepin.1@umontreal.ca>
2024-06-26 22:44:49 -0700

    Added tchopetchope project to website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,18 @@ export default defineNuxtConfig({
     baseURL: '',
     buildAssetsDir: 'assets',
   },
+  content: {
+    highlight: {
+      theme: {
+        // Default theme (same as single string)
+        default: 'github-light',
+        // Theme used if `html.dark`
+        dark: 'github-dark',
+        // Theme used if `html.sepia`
+        sepia: 'monokai'
+      }
+    }
+  },
   // https://github.com/nuxt-themes/alpine
   extends: '@nuxt-themes/alpine',
 
